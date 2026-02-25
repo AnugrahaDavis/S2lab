3692b7470466ee79dd4c0ac84ab175d2cb4dbc26
bhavanatheruvath <bhavanatheruvath2255@gmail.com>
2026-02-25 13:28:29 +0530

    java

--- java/cpu.java
+++ java/cpu.java
@@ -1,4 +1,56 @@
-public class cpu {
-    
-    
+public class CPU {
+
+    double price;
+
+ 
+    CPU(double price) {
+        this.price = price;
+    }
+
+ 
+    class Processor {
+        int numberOfCores;
+        String manufacturer;
+
+        Processor(int numberOfCores, String manufacturer) {
+            this.numberOfCores = numberOfCores;
+            this.manufacturer = manufacturer;
+        }
+
+        void displayProcessor() {
+            System.out.println("Processor Information:");
+            System.out.println("Number of Cores: " + numberOfCores);
+            System.out.println("Manufacturer: " + manufacturer);
+        }
+    }
+
+    // Static Nested Class
+    static class RAM {
+        int memory;
+        String manufacturer;
+
+        RAM(int memory, String manufacturer) {
+            this.memory = memory;
+            this.manufacturer = manufacturer;
+        }
+
+        void displayRAM() {
+            System.out.println("RAM Information:");
+            System.out.println("Memory: " + memory + "GB");
+            System.out.println("Manufacturer: " + manufacturer);
+        }
+    }
+
+    // Main method inside CPU class
+    public static void main(String[] args) {
+
+        CPU cpu = new CPU(50000);
+
+        CPU.Processor processor = cpu.new Processor(6, "Intel");
+        CPU.RAM ram = new CPU.RAM(16, "Kingston");
+
+        System.out.println("CPU Price: " + cpu.price);
+        processor.displayProcessor();
+        ram.displayRAM();
+    }
 }
