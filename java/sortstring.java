70f14cdbfb5111c5549ccc624f0e8da8f15bc2e2
bhavanatheruvath <bhavanatheruvath2255@gmail.com>
2026-03-23 14:20:54 +0530

    java

--- java/sortstring.java
+++ java/sortstring.java
@@ -2,13 +2,21 @@ import java.util.*;
 public class sortstring {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string");
         String str=sc.nextLine();
         char[] arr = str.toCharArray();
 
         for(int i=0;i<arr.length-1;i++){
-            for(int j=0;j<arr.length-i-1;)
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    char temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
         }
-
+        String sorted=new String(arr);
+        System.out.println(sorted);
         
     } 
 }
