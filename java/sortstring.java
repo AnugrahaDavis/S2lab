69014eae3356fc9deb506d5d3b685297f4e54e8b
bhavanatheruvath <bhavanatheruvath2255@gmail.com>
2026-03-23 14:03:08 +0530

    java

---
+++ java/sortstring.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class sortstring {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        char[] arr = str.toCharArray();
+
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;)
+        }
+
+        
+    } 
+}
+
